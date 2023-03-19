808dce33cadadb628c0a30dff739cf5315d4396b
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-03-19 03:29:33 -0300

    feat(search_engine): implement search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,9 @@
 from tech_news.database import search_news
+from datetime import datetime
+
+
+def normalize_news(news_list):
+    return [(news['title'], news['url']) for news in news_list]
 
 
 # Requisito 7
@@ -6,12 +11,19 @@ def search_by_title(title):
     """Seu código deve vir aqui"""
     query = {"title": {"$regex": title, "$options": "-i"}}
     news_list = search_news(query)
-    return [(news['title'], news['url']) for news in news_list]
+    return normalize_news(news_list)
 
 
 # Requisito 8
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        formattedDate = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        query = {"timestamp": {"$regex": formattedDate, "$options": "i"}}
+        news_list = search_news(query)
+        return normalize_news(news_list)
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
