078e3c25e8473b2a6ee9076363f94d0cfe357e04
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-03-19 03:34:12 -0300

    fix: moved test_reading_plan mock to the test scope

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -6,30 +6,28 @@ from tests.assets.news import NEWS
 from unittest.mock import patch
 
 
-EXPECTED_RETURN = {
-    "readable": [
-        {
-            "unfilled_time": 1,
-            "chosen_news": [("noticia_0", 2), ("Notícia bacana", 4)],
-        },
-        {
-            "unfilled_time": 2,
-            "chosen_news": [
-                ("Notícia bacana 2", 1),
-                ("noticia_3", 1),
-                ("noticia_4", 1),
-                ("noticia_5", 1),
-                ("noticia_6", 1),
-            ],
-        },
-        {"unfilled_time": 0, "chosen_news": [("noticia_7", 7)]},
-        {"unfilled_time": 2, "chosen_news": [("noticia_9", 5)]},
-    ],
-    "unreadable": [("noticia_8", 8)],
-}
-
-
 def test_reading_plan_group_news():
+    EXPECTED_RETURN = {
+        "readable": [
+            {
+                "unfilled_time": 1,
+                "chosen_news": [("noticia_0", 2), ("Notícia bacana", 4)],
+            },
+            {
+                "unfilled_time": 2,
+                "chosen_news": [
+                    ("Notícia bacana 2", 1),
+                    ("noticia_3", 1),
+                    ("noticia_4", 1),
+                    ("noticia_5", 1),
+                    ("noticia_6", 1),
+                ],
+            },
+            {"unfilled_time": 0, "chosen_news": [("noticia_7", 7)]},
+            {"unfilled_time": 2, "chosen_news": [("noticia_9", 5)]},
+        ],
+        "unreadable": [("noticia_8", 8)],
+    }
     with patch("tech_news.analyzer.reading_plan.find_news", return_value=NEWS):
         mocked_return = ReadingPlanService.group_news_for_available_time(7)
         assert (
